f3fef2bf7b58cd41932d6143bca8294c57e0ec23
Javier Dario Echavarria Cano <jdechavarriac@unal.edu.co>
2020-03-11 13:09:01 -0500

    Final algorithm

--- EmbeddedAssemblerInCpp.cpp
+++ EmbeddedAssemblerInCpp.cpp
@@ -1,53 +1,96 @@
+
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
 
+int main() {
+	long double x;
+	long double a;
+	long int m;
 
-int main(){
-	double x;
-	double a;
-	double m;
-	double resultado = 0;
-
-	cout << "ingrese el valor de \"x\" para el cual se calculara el valor de arctan(x/a): ";
+	cout << "ingrese el valor de \"x\" para el cual se calculara arctan(x/a): ";
 	cin >> x;
-	cout << "x=" << x << "\n";
 	
-	cout << "ingrese el valor del divisor \"a\" para el cual se calculara el valor de arctan(x/a): ";
+
+	cout << "ingrese el valor del divisor \"a\" para el cual se calculara arctan(x/a): ";
 	cin >> a;
-	cout << "a=" << a << "\n";
 	
-	cout << "x/a=" << x / a << "\n";
-	
+
 	cout << "ingrese el numero de terminos \"m\" que se usaran para calcular la serie: ";
 	cin >> m;
-	cout << "m=" << m << "\n";
 	
-	double num = x / a;
-	double res = 1;
-	for (int i = 1; i <= m; i++)
-	{
-		res = -1 * res;
+	cout << "x=" << x << "\n";
+	cout << "a=" << a << "\n";
+	cout << "m=" << m << "\n";
+
+	long double pot = -1;
+	long double res = 1;
+	long double resultado = 0;
+	long double cociente;
+	long int tempint;
+
+	_asm {
+		finit
+		fld x						//*
+		fld a						//*Con la FPU se calcula el valor de (x/a)
+		fdiv						//*
+		fstp cociente
+		fwait
+		//inicio while 1
+		mov eax, m					//*
+		inc eax						//*While1 irá desde 0 hasta m, por lo tanto el límite tendrá que ser m+1
+		mov m, eax					//*
+		
+		mov ecx, 0					//inicializamos el contador ecx para el primer while.
+		while1:	
+		cmp ecx, m					//while1 va desde 0 hasta m
+		jge fin_while1
+			
+			mov eax, 2				//*
+			mov ebx, ecx			//*Calculamos el valor de 2n+1 en el registro eax
+			mul ebx					//*
+			inc eax					//*
+
+			mov edx, 1				//edx es el contador de while2, comienza en 1 y va hasta 2n+1
+			fld cociente
+
+			while2:
+			cmp edx, eax			//el segundo while va desde 1 hasta 2n+1
+				jge fin_while2
+
+				fld cociente		//*
+				fmul				//*multiplicamos (x/a) por sí misma 2n+1 veces
+				inc edx				//incrementamos el contador de while2
+				jmp while2
+			fin_while2:
+			//fin while 2
+			fld pot					//*
+			fchs					//*Se le cambia el signo a 1, equivalente a (-1)^n
+			fst pot					//*
+
+			mov tempint, ecx		//*
+			fild tempint			//*
+			mov tempint, 2			//*
+			fild tempint			//*
+			fmul					//*calculamos 2n+1
+			mov tempint, 1			//*
+			fild tempint			//*
+			fadd					//*	
+			fdiv					//*
+			fmul					//*
+
+			fld resultado			//*
+			fadd					//* resultado += (x/a)*(-1)^n/(2n+1)
+			fstp resultado			//*
+
+			inc ecx					//incrementamos el contador de while1
+			jmp while1
+		fin_while1:
+		//fin while 1
+		fwait
 	}
 
-	cout << "resultado de -1^m: " << res << "\n";
-
-	res = 1;
-	for (int i = 1; i <= ((2*m)+1); i++)
-	{
-		res = res * num;
-	}
-
-
-	cout << "resultado de (x/a)^(2m+1): " << res << "\n";
-
-	cout << "resultado de 1/(2m+1): " << 1 / ((2 * m) + 1) << "\n";
-
-	_asm
-	{
-		/*aqui va todo el codigo ensamblador*/
-	}
+	cout << "arctan(" << x << "/" << a << ") = " << resultado << "rad" << "\n";
 
 	system("pause");
 }
